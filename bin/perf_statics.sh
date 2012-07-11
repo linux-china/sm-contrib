f8593c9a35250b5551ecfe928998f884643bfea6
linux_china <libing.chen@gmail.com>
2012-07-11 16:46:28 +0800

    minor update

--- bin/perf_statics.sh
+++ bin/perf_statics.sh
@@ -4,8 +4,8 @@ declare DATE=`date +%m/%d/%Y`
 declare TIME=`date +%k:%M:%S`
 declare USERS=`uptime | sed 's/users.*$//' | gawk '{print $NF}'`
 declare LOAD=`uptime | gawk '{print $NF}'`
-declare FREE=`/usr/bin/vm_stat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $4}'`
-declare IDLE=`/usr/bin/vm_stat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $15}'`
+declare FREE=`/usr/bin/vmstat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $4}'`
+declare IDLE=`/usr/bin/vmstat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $15}'`
 #########################################
 # Send Statistics
 #########################################
