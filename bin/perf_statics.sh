9cca991ec12bba6206dea6454faab48a5798b8ee
linux_china <libing.chen@gmail.com>
2012-07-11 16:40:39 +0800

    add declare for variables

--- bin/perf_statics.sh
+++ bin/perf_statics.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 ##################################################
-DATE=`date +%m/%d/%Y`
-TIME=`date +%k:%M:%S`
-USERS=`uptime | sed 's/users.*$//' | gawk '{print $NF}'`
-LOAD=`uptime | gawk '{print $NF}'`
-FREE=`/usr/bin/vm_stat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $4}'`
-IDLE=`/usr/bin/vm_stat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $15}'`
+declare DATE=`date +%m/%d/%Y`
+declare TIME=`date +%k:%M:%S`
+declare USERS=`uptime | sed 's/users.*$//' | gawk '{print $NF}'`
+declare LOAD=`uptime | gawk '{print $NF}'`
+declare FREE=`/usr/bin/vm_stat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $4}'`
+declare IDLE=`/usr/bin/vm_stat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $15}'`
 #########################################
 # Send Statistics
 #########################################
 echo "=================="
-echo "DATE: ==> $DATE"
-echo "TIME: ==> $TIME"
-echo "USERS: ==> $USERS"
-echo "LOAD: ==> $LOAD"
-echo "FREE: ==> $FREE"
-echo "IDLE: ==> $IDLE"
+echo "DATE: ==> ${DATE}"
+echo "TIME: ==> ${TIME}"
+echo "USERS: ==> ${USERS}"
+echo "LOAD: ==> ${LOAD}"
+echo "FREE: ==> ${FREE}"
+echo "IDLE: ==> ${IDLE}"
 echo "=================="
